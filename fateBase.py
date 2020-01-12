0f941bf472b03a65e51c77621095c1b930fc7325
Myrrhion <gbuessin@students.uni-mainz.de>
2020-01-12 12:33:19 +0100

    testymcface added

--- fateBase.py
+++ fateBase.py
@@ -3,7 +3,7 @@ import re
 import asyncio
 
 class Aspect:
-	def __init__(self,aspect_description,free_invokes=[]):
+	def __init__(self,parent,aspect_description,free_invokes=[]):
 		self.owner = parent
 		self.description = aspect_description
 		self.free_invokes = free_invokes
@@ -22,8 +22,11 @@ class Aspect:
 	def refresh(self):
 		self.invoked = False
 	def __repr__(self):
-		return f"Aspect(name: {self.description}, free invokations: {self.free_invokes})"
-
+		return f"{self.__class__.__name__}(name: {self.description}, free invokations: {[str(x) for x in self.free_invokes]})"
+class SignatureAspect(Aspect):
+	@classmethod
+	def upgrade(cls,original):
+		return cls(original.owner,original.description,[original.owner])
 class StressBar:
 	def __init__(self,parent,name,maximum_stress):
 		self.character = parent
@@ -41,16 +44,25 @@ class StressBar:
 		return f"StressBar(name:{self.name},boxes:{self.stress_boxes})"
 
 class Skill:
-	def __init__(self,name,actions:dict,value:int):
+	def __init__(self,parent,name,actions:dict,value:int):
+		self.character = parent
 		self.name = name
 		self.actions = actions
-		self.value = value
+		self.value = int(value)
+		parent.skills[name] = self
 	def __str__(self):
 		return self.name
 	def __getkey__(self,key):
 		return self.actions[key]
 	def __int__(self):
 		return self.value
+	def __iter__(self):
+		return self.actions
+	def can_do(self,action):
+		for x in self:
+			if x.beginswith(action):
+				return True
+		return False
 	def __repr__(self):
 		return f"Skill(name: {self.name}, value: {self.value}, actions: {repr(self.actions)})"
 
@@ -81,7 +93,7 @@ class Consequence:
 	def __repr__(self):
 		return f"Consequence(name: {self.name}, type:{self.type}, value:{self.value})"
 
-def get_aspect_list(text):
+def get_aspect_list(text,owner):
 	aspects = {}
 	for x in re.findall(r"\*\*([^*]*)\*\*",text):
 		aspects[x] = Aspect(x)
@@ -96,6 +108,56 @@ class FateCharacterBase:
 		for x in aspects:
 			self.aspects[x] = Aspect(x)
 
+class Stunt:
+	def __init__(self,parent,name,description,**kwargs):
+		self.character = parent
+		self.description = description
+		self.name = name
+	def __str__(self):
+		return self.name
+	def __repr__(self):
+		return f"{self.__class__.__name__}(name: {self.name}, description: {self.description})"
+class ImproveStunt(Stunt,Skill):
+	def __init__(self,parent,name,description,associated_skill,actions:list,boost=2,**kwargs):
+		Stunt.__init__(self,parent,name,description)
+		self._skill = parent.skills[associated_skill]
+		for a in actions:
+			if a not in self._skill.actions:
+				raise ValueError("NOPE")
+		self.actions = actions
+		self._boost = boost
+		parent.skills[str(self)] = self
+	def __int__(self):
+		return int(self._skill) + self._boost
+	def __repr__(self):
+		return f"ImproveStunt(name: {self.name}, description: {self.description})"
+class ExtendStunt(Stunt):
+	def __init__(self,parent,name,description,associated_skill,action,**kwargs):
+		Stunt.__init__(self,parent,name,description)
+		self._skill = associated_skill
+		self.action = action
+		parent.skills[str(self)] = self
+	def __int__(self):
+		return int(self._skill)
+	def can_do(self,action):
+		return self.action.beginswith(action)
+class SignatureAspectStunt(Stunt):
+	def __init__(self,parent,name,**kwargs):
+		Stunt.__init__(self,parent,name,"Signature Aspect")
+		self.character = parent
+		self.name = name
+		newb = None
+		for aspect in parent.char_aspects:
+			if name == aspect:
+				newb= aspect
+		
+		parent.char_aspects.remove(newb)
+		parent.char_aspects.append(SignatureAspect.upgrade(newb))
+
+class FatePlayerBase:
+	def __init__(self,player,**kwarg):
+		self.player = player
+		self.char_aspects = []
 class FateBase(fudge.base.RollSystem):
 	def __init__(self,dm,channel):
 		fudge.base.RollSystem.__init__(self,dm,channel)
@@ -115,6 +177,6 @@ class FateBase(fudge.base.RollSystem):
 		if command.beginswith("!overcome "):
 			pass
 	def make_scene(self, command):
-		self.scene_aspects = get_aspect_list(command)
+		self.scene_aspects = get_aspect_list(command,self)
 	def extend_scene(self, command):
-		self.scene_aspects.update(get_aspect_list(command))
+		self.scene_aspects.update(get_aspect_list(command,self))
--- fateBendy.py
+++ fateBendy.py
@@ -1,40 +1,65 @@
-import fateBase
+from fateBase import *
 import json
 
-class FateBendyCharacter(fateBase.FateCharacterBase):
-	def __init__(self):
-		pass
+class FateBendyCharacter(FateCharacterBase):
+	def __init__(self,player,**kwargs):
+		fateBase.FateCharacterBase.__init__(self,player,**kwargs)
 
-class FateBendy(fateBase.FateBase):
+
+class FatePlayerBendy(FatePlayerBase):
+	def __init__(self,player,**kwargs):
+		FatePlayerBase.__init__(self,player,**kwargs)
+		self.core_aspect = Aspect(self,kwargs.get("core aspect"))
+		self.omega_aspect = Aspect(self,kwargs.get("omega aspect"))
+		self.skills = {}
+		self.char_aspects = [self.core_aspect,self.omega_aspect]
+		self.average_mode_two = Mode(self, value = 1, **kwargs.get("average mode two"))
+		self.average_mode_one = Mode(self, value =1, **kwargs.get("average mode one"))
+		self.fair_mode = Mode(self,value=2,**kwargs.get("fair mode"))
+		self.good_mode = Mode(self,value=3,**kwargs.get("good mode"))
+		self.stunts = []
+		for x in kwargs.get("stunts",[]):
+			exec(f"self.stunts.append({x['type']}(self,**x))")
+	def __str__(self):
+		return str(self.player)
+	def __repr__(self):
+		return f"FatePlayerBendy(name: {self.player}, core_aspect: {self.core_aspect}, aspects:{self.char_aspects}, skills:{list(self.skills[x] for x in self.skills)}, stunts: {self.stunts})"
+
+class FateBendy(FateBase):
 	skills = json.load(open("data/BendySkills.txt"))
 
 
 class Mode:
 	def __init__(self, parent:FateBendyCharacter, name:str, value:int, skills:dict):
-		self.name = fateBase.Aspect(name)
-		self.value = value
+		self.name = Aspect(parent,name)
+		parent.char_aspects.append(self.name)
+		self.value = int(value)
 		self.character = parent
 		self.skills = {}
 		for x in skills:
-			self.skill[x] = BendySkill.build_from_name(x, skills[x],self)
+			self.skills[x] = BendySkill.build_from_name(x, skills[x],self)
 	def rename(self,new_name:str):
-		self.name = fateBase.Aspect(name, self.name.free_invokes)
+		self.name = Aspect(self,name, self.name.free_invokes)
 		self.character.state_changed()
 		return self
+	def __str__(self):
+		return str(self.name)
 	@classmethod
-	def modeInkling(cls):
-		pass
+	def modeInkling(cls,character):
+		return cls(character,"")
 	
 
-class BendySkill(fateBase.Skill):
-	def __init__(self, name, actions:dict,value:int,mode:Mode = None):
-		fateBase.Skill.__init__(self,name,actions,value)
+class BendySkill(Skill):
+	def __init__(self, name, mode, actions:dict,value:int):
+		Skill.__init__(self,mode.character,name,actions,value)
 		self.mode = mode
 	def __len__(self):
 		return len(self.actions)
 	def __int__(self):
 		return self.value + self.mode.value
+	def __repr__(self):
+		return f"Skill(name: {self.name}, mode: {str(self.mode)}, value: {int(self)}, actions: {repr(self.actions)})"
 	@classmethod
 	def build_from_name(cls,name:str,value:int,mode:Mode = None):
-		return cls(name, FateBendy.skills[name], value, mode)
-print(FateBendy.skills)
+		return cls(name,mode, FateBendy.skills[name], value)
+print(repr(FatePlayerBendy("Dingus",**json.load(open("data/Bendy/Players/Testymcface.json")))))
